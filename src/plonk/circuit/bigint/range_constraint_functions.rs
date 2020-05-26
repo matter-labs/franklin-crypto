a33987abdb90d76b3ac2a57cfe6785504c9793bb
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-05-26 18:08:22 +0300

    add tests when width is not an exact multiple

--- src/plonk/circuit/bigint/range_constraint_functions.rs
+++ src/plonk/circuit/bigint/range_constraint_functions.rs
@@ -78,9 +78,10 @@ pub fn coarsely_enforce_using_multitable<E: Engine, CS: ConstraintSystem<E>>(
 
     let num_full_gates = num_bits / width_per_gate;
     let mut num_terms_in_partial_gate = 0;
-    if num_bits % width_per_gate != 0 {
-        num_terms_in_partial_gate = num_bits / minimal_per_gate;
-        if num_bits % minimal_per_gate != 0 {
+    let remainder_bits = num_bits % width_per_gate;
+    if remainder_bits != 0 {
+        num_terms_in_partial_gate = remainder_bits / minimal_per_gate;
+        if remainder_bits % minimal_per_gate != 0 {
             num_terms_in_partial_gate += 1;
         }
     }
@@ -98,15 +99,13 @@ pub fn coarsely_enforce_using_multitable<E: Engine, CS: ConstraintSystem<E>>(
     let mut next_step_value = None;
 
     for full_gate_idx in 0..num_full_gates {
-        cs.begin_gates_batch_for_step()?;
-
         if next_step_value.is_none() {
             next_step_value = Some(E::Fr::zero());
         }
 
         let mut term = MainGateTerm::<E>::new();
         for _ in 0..3 {
-            let value = it.next().unwrap();
+            let value = (&mut it).next().unwrap();
             let allocated = AllocatedNum::alloc(cs, || {
                 Ok(*value.get()?)
             })?;
@@ -118,10 +117,6 @@ pub fn coarsely_enforce_using_multitable<E: Engine, CS: ConstraintSystem<E>>(
 
             current_term_coeff.mul_assign(&shift);
         }
-
-        let next_var = AllocatedNum::alloc(cs, || {
-            Ok(*next_step_value.get()?)
-        })?;
 
         if let Some(from_previous) = next_step_variable_from_previous_gate.take() {
             term.add_assign(ArithmeticTerm::from_variable(from_previous.get_variable()));
@@ -136,10 +131,15 @@ pub fn coarsely_enforce_using_multitable<E: Engine, CS: ConstraintSystem<E>>(
         let is_last_gate = (full_gate_idx == num_full_gates - 1) && num_terms_in_partial_gate == 0;
 
         if is_last_gate == false {
+            let next_var = AllocatedNum::alloc(cs, || {
+                Ok(*next_step_value.get()?)
+            })?;
             next_step_variable_from_previous_gate = Some(next_var);
         } else {
             next_step_variable_from_previous_gate = Some(to_constraint.clone());
         }
+
+        cs.begin_gates_batch_for_step()?;
 
         cs.new_gate_in_batch(
             &CS::MainGate::default(), 
@@ -155,15 +155,52 @@ pub fn coarsely_enforce_using_multitable<E: Engine, CS: ConstraintSystem<E>>(
         cs.end_gates_batch_for_step()?;
     }
 
-    // add final gate to allocate D_next
+    // make at most one gate where not all the inputs are usefull
+    if num_terms_in_partial_gate != 0 {
+        let mut term = MainGateTerm::<E>::new();
+        for value in it {
+            let allocated = AllocatedNum::alloc(cs, || {
+                Ok(*value.get()?)
+            })?;
 
-    // let mut term = MainGateTerm::<E>::new();
-    // for _ in 0..linear_terms_used {
-    //     term.add_assign(ArithmeticTerm::<E>::from_variable_and_coeff(dummy_var, E::Fr::zero()));
-    // }
-    // term.add_assign(ArithmeticTerm::<E>::from_variable_and_coeff(final_val.get_variable(), E::Fr::zero()));
+            let scaled = value.mul(&Some(current_term_coeff));
+            next_step_value = next_step_value.add(&scaled);
 
-    // cs.allocate_main_gate(term)?;
+            term.add_assign(ArithmeticTerm::from_variable_and_coeff(allocated.get_variable(), current_term_coeff));
+
+            current_term_coeff.mul_assign(&shift);
+        }
+
+        for _ in num_terms_in_partial_gate..linear_terms_used {
+            term.add_assign(ArithmeticTerm::from_variable_and_coeff(explicit_zero_var, E::Fr::zero()));
+        }
+
+        if let Some(from_previous) = next_step_variable_from_previous_gate.take() {
+            term.add_assign(ArithmeticTerm::from_variable(from_previous.get_variable()));
+        } else {
+            term.add_assign(ArithmeticTerm::from_variable(explicit_zero_var));
+        }
+
+        let (variables, mut coeffs) = CS::MainGate::format_linear_term_with_duplicates(term, dummy_var)?;
+        coeffs[next_step_coeff_idx] = minus_one;
+        
+        next_step_variable_from_previous_gate = Some(to_constraint.clone());
+
+        cs.begin_gates_batch_for_step()?;
+
+        cs.new_gate_in_batch(
+            &CS::MainGate::default(), 
+            &coeffs, 
+            &variables, 
+            &[]
+        )?;
+
+        let table = cs.get_multitable(RANGE_CHECK_MULTIAPPLICATION_TABLE_NAME)?;
+
+        cs.apply_multi_lookup_gate(&variables[0..linear_terms_used], table)?;
+
+        cs.end_gates_batch_for_step()?;
+    }
 
     let final_val = next_step_variable_from_previous_gate.unwrap();
 
@@ -213,6 +250,69 @@ mod test {
             &mut cs,
             &num,
             48
+        ).unwrap();
+
+        assert!(cs.n() == 2);
+
+        assert!(cs.is_satisfied());
+    }
+
+    #[test]
+    fn make_coarse_case_range_constraint() {
+        type E = crate::bellman::pairing::bn256::Bn256;
+        type Fr = crate::bellman::pairing::bn256::Fr;
+
+        let mut cs = TrivialAssembly::<E, Width4WithCustomGates, Width4MainGateWithDNext>::new();
+
+        let over = vec![PolyIdentifier::VariablesPolynomial(0), PolyIdentifier::VariablesPolynomial(1), PolyIdentifier::VariablesPolynomial(2)];
+        let table = MultiTableApplication::<E>::new_range_table_of_width_3(16, over).unwrap();
+
+        cs.add_multitable(table).unwrap();
+
+        let value = Fr::from_str(&"123456").unwrap();
+        let num = AllocatedNum::alloc(
+            &mut cs,
+            || {
+                Ok(value)
+            }
+        ).unwrap();
+
+        coarsely_enforce_using_multitable(
+            &mut cs,
+            &num,
+            60
+        ).unwrap();
+
+        assert!(cs.n() == 3);
+
+        assert!(cs.is_satisfied());
+    }
+
+
+    #[test]
+    fn make_coarse_short_range_constraint() {
+        type E = crate::bellman::pairing::bn256::Bn256;
+        type Fr = crate::bellman::pairing::bn256::Fr;
+
+        let mut cs = TrivialAssembly::<E, Width4WithCustomGates, Width4MainGateWithDNext>::new();
+
+        let over = vec![PolyIdentifier::VariablesPolynomial(0), PolyIdentifier::VariablesPolynomial(1), PolyIdentifier::VariablesPolynomial(2)];
+        let table = MultiTableApplication::<E>::new_range_table_of_width_3(16, over).unwrap();
+
+        cs.add_multitable(table).unwrap();
+
+        let value = Fr::from_str(&"123456").unwrap();
+        let num = AllocatedNum::alloc(
+            &mut cs,
+            || {
+                Ok(value)
+            }
+        ).unwrap();
+
+        coarsely_enforce_using_multitable(
+            &mut cs,
+            &num,
+            20
         ).unwrap();
 
         assert!(cs.n() == 2);
