8ce3d96e1de8c878b19b2159197329abb87de923
Konstantce <kopanarin@yandex.ru>
2020-10-27 14:16:29 +0300

    with byte interface

--- src/plonk/circuit/hashes_with_tables/sha256/test.rs
+++ src/plonk/circuit/hashes_with_tables/sha256/test.rs
@@ -9,9 +9,15 @@ mod test {
         AllocatedNum,
         Num,
     };
+    use crate::plonk::circuit::byte::{
+        Byte,
+    };
     use crate::bellman::pairing::bn256::{Bn256, Fr};
 
     use super::super::gadgets::*;
+    use super::super::utils::*;
+    use super::super::super::utils::*;
+
     use rand::{Rng, SeedableRng, StdRng};
 
 
@@ -21,6 +27,7 @@ mod test {
         ch_base_num_of_chunks: Option<usize>,
         maj_sheduler_base_num_of_chunks: Option<usize>,
         is_const_test: bool,
+        is_byte_test: bool,
     }
 
     impl<E: Engine> Circuit<E> for TestSha256Circuit<E> {
@@ -35,17 +42,6 @@ mod test {
         }
 
         fn synthesize<CS: ConstraintSystem<E>>(&self, cs: &mut CS) -> Result<(), SynthesisError> {
-
-            let mut input_vars = Vec::with_capacity(16);
-            for value in self.input.iter() {
-                if !self.is_const_test {
-                    let new_var = AllocatedNum::alloc(cs, || Ok(value.clone()))?;
-                    input_vars.push(Num::Variable(new_var));
-                }
-                else {
-                    input_vars.push(Num::Constant(value.clone()));
-                }
-            }
 
             let mut actual_output_vars = Vec::with_capacity(16);
             for value in self.output.iter() {
@@ -62,7 +58,34 @@ mod test {
                 cs, self.ch_base_num_of_chunks, self.maj_sheduler_base_num_of_chunks, false, false, 0, "",
             )?;
 
-            let supposed_output_vars = sha256_gadget.sha256(cs, &input_vars[..])?;
+            let supposed_output_vars = if !self.is_byte_test {    
+                let mut input_vars = Vec::with_capacity(self.input.len());
+                for value in self.input.iter() {
+                    if !self.is_const_test {
+                        let new_var = AllocatedNum::alloc(cs, || Ok(value.clone()))?;
+                        input_vars.push(Num::Variable(new_var));
+                    }
+                    else {
+                        input_vars.push(Num::Constant(value.clone()));
+                    }
+                }
+                sha256_gadget.sha256(cs, &input_vars[..])?
+            }
+            else {
+                let mut input_vars = Vec::with_capacity(self.input.len());
+                for value in self.input.iter() {
+                    if !self.is_const_test {
+                        let new_var = AllocatedNum::alloc(cs, || Ok(value.clone()))?;
+                        let byte = Byte::from_num_unconstrained(cs, Num::Variable(new_var));
+                        input_vars.push(byte);
+                    }
+                    else {
+                        let byte = Byte::from_cnst(cs, value.clone());
+                        input_vars.push(byte);
+                    }
+                }
+                sha256_gadget.sha256_from_bytes(cs, &input_vars[..])?
+            };
 
             for (a, b) in supposed_output_vars.iter().zip(actual_output_vars.into_iter()) {
                 a.eq(cs, &b)?;
@@ -122,6 +145,7 @@ mod test {
             ch_base_num_of_chunks: None,
             maj_sheduler_base_num_of_chunks: None,
             is_const_test: false,
+            is_byte_test: false,
         };
 
         let mut assembly = TrivialAssembly::<Bn256, PlonkCsWidth4WithNextStepParams, Width4MainGateWithDNext>::new();
@@ -174,6 +198,7 @@ mod test {
             ch_base_num_of_chunks: None,
             maj_sheduler_base_num_of_chunks: None,
             is_const_test: false,
+            is_byte_test: false,
         };
 
         let mut assembly = TrivialAssembly::<Bn256, PlonkCsWidth4WithNextStepParams, Width4MainGateWithDNext>::new();
@@ -226,6 +251,7 @@ mod test {
             ch_base_num_of_chunks: None,
             maj_sheduler_base_num_of_chunks: None,
             is_const_test: true,
+            is_byte_test: false,
         };
 
         let mut assembly = TrivialAssembly::<Bn256, PlonkCsWidth4WithNextStepParams, Width4MainGateWithDNext>::new();
@@ -236,8 +262,49 @@ mod test {
         assert!(assembly.is_satisfied());
     }
 
-    // fn polished_sha256_from_bytes_test()
-    // {
+    #[test]
+    fn polished_sha256_gadget_bytes_test() 
+    {
+        const NUM_OF_BYTES: usize = 341;
+        const IS_CONST_TEST: bool = false;
 
-    // }
+        let mut rng = rand::thread_rng();
+
+        let mut input = [0u8; NUM_OF_BYTES];
+        for i in 0..NUM_OF_BYTES {
+            input[i] = rng.gen();
+        }
+    
+        // create a Sha256 object
+        let mut hasher = Sha256::new();
+        // write input message
+        hasher.input(&input[..]);
+        // read hash digest and consume hasher
+        let output = hasher.result();
+
+        let mut input_fr_arr : Vec<<Bn256 as ScalarEngine>::Fr> = Vec::with_capacity(NUM_OF_BYTES);
+        let mut output_fr_arr = [Fr::zero(); 8];
+
+        input_fr_arr.extend(input.into_iter().map(|byte| u64_to_ff::<<Bn256 as ScalarEngine>::Fr>(*byte as u64)));
+        
+        for (i, block) in output.chunks(4).enumerate() {
+            output_fr_arr[i] = slice_to_ff::<Fr>(block);
+        }
+        
+        let circuit = TestSha256Circuit::<Bn256>{
+            input: input_fr_arr,
+            output: output_fr_arr,
+            ch_base_num_of_chunks: None,
+            maj_sheduler_base_num_of_chunks: None,
+            is_const_test: IS_CONST_TEST,
+            is_byte_test: true,
+        };
+
+        let mut assembly = TrivialAssembly::<Bn256, PlonkCsWidth4WithNextStepParams, Width4MainGateWithDNext>::new();
+
+        circuit.synthesize(&mut assembly).expect("must work");
+        println!("Assembly contains {} gates", assembly.n());
+        println!("Total length of all tables: {}", assembly.total_length_of_all_tables);
+        assert!(assembly.is_satisfied());
+    }
 }
--- src/plonk/circuit/hashes_with_tables/sha256/utils.rs
+++ src/plonk/circuit/hashes_with_tables/sha256/utils.rs
@@ -1,105 +1,9 @@
 use crate::bellman::pairing::ff::*;
-use crate::num_bigint::BigUint;
-use crate::num_traits::cast::ToPrimitive;
-use crate::num_traits::{ Zero, One };
-use std::{ iter, mem };
-
+use super::super::utils::*;
 
 pub const SHA256_CHOOSE_BASE: u64 = 7;
 pub const SHA256_MAJORITY_SHEDULER_BASE: u64 = 4;
 
-
-pub fn pow(base: usize, exp: usize) -> usize {
-
-    let mut res = 1;
-    for _i in 0..exp {
-        res *= base;
-    }
-
-    res
-}
-
-pub fn biguint_pow(base: usize, exp: usize) -> BigUint {
-    let mut res = BigUint::one();
-    for _i in 0..exp {
-        res *= base;
-    }
-
-    res
-}
-
-pub fn u64_to_ff<Fr: PrimeField>(n: u64) -> Fr {
-    let mut repr : <Fr as PrimeField>::Repr = Fr::zero().into_repr();
-    repr.as_mut()[0] = n;
-    let res = Fr::from_repr(repr).expect("should parse");
-    res
-}
-
-pub fn u64_exp_to_ff<Fr: PrimeField>(n: u64, exp: u64) -> Fr {
-    let mut repr : <Fr as PrimeField>::Repr = Fr::zero().into_repr();
-    repr.as_mut()[0] = n;
-    let mut res = Fr::from_repr(repr).expect("should parse");
-
-    if exp != 0 {
-        res = res.pow(&[exp]);
-    }
-    res
-}
-
-pub fn ff_to_u64<Fr: PrimeField>(fr: &Fr) -> u64 {
-    let repr = fr.into_repr();
-    for i in 1..4 {
-        assert_eq!(repr.as_ref()[i], 0);
-    }
-    repr.as_ref()[0]
-}
-
-fn biguint_to_ff<Fr: PrimeField>(value: &BigUint) -> Fr {
-    Fr::from_str(&value.to_str_radix(10)).unwrap()
-}
-
-
-// converts x = (x_0, x_1, ..., x_n) - bit decomposition of x into y = \sum_{i=1}^{n} x_i * base^i
-pub fn map_into_sparse_form(input: usize, base: usize) -> BigUint
-{
-    // if base is zero, than no convertion is actually needed
-    if base == 0 {
-        return BigUint::from(input);
-    }
-    
-    let mut out = BigUint::zero();
-    let mut base_accumulator = BigUint::one();
-    let mut converted = input;
-
-    while converted > 0 {
-        let sparse_bit = converted & 1;
-        converted >>= 1;
-        if sparse_bit > 0 {
-            out += base_accumulator.clone();
-        }
-        base_accumulator *= base;
-    }
-    
-    out
-}
-
-pub fn map_from_sparse_form(input: usize, base: usize) -> usize
-{
-    let mut target : usize = input;
-    let mut output : usize = 0;
-    let mut count : usize = 0;
-
-    while target > 0 {
-        let slice = target % base;
-        // althoough slice is not bound to {0, 1} we are only interested in its value modulo 2
-        let bit = slice & 1usize;
-        output += bit << count;
-        count += 1;
-        target = target / base;
-    }
-
-    output
-}
 
 // for given x=(x_0, x_1, ..., x_n) extract the k lower bits: y = (x_0, x_1, ..., x_{k-1}, 0, ..., 0)
 // and then rotate
@@ -117,40 +21,6 @@ pub fn shift_right(value: usize, shift: usize) -> usize
     if shift > 0 {value >> shift} else {value}
 }
 
-
-// in general all bit-friendly transformations (such as xor or SHA256 majority and choose operations)
-// are hadled by the gadget library itself with the use of tables
-// however, in some our wires are actually constants and not allocated variables
-// for them we do not want apply the common strategy 
-// ( split in chunks -- chunk-by-chunk transform via tables -- compose chunks back )
-// but want to do all the bitwise transformations on the whole constant number in one turn
-// this is done by the corresponding "normalizer" mappings, contained in this module
-fn general_normalizer<Fr: PrimeField>(fr : Fr, bit_table: &[u64], base: u64) -> Fr
-{
-    let mut input = BigUint::default();
-    let fr_repr = fr.into_repr();
-    for n in fr_repr.as_ref().iter().rev() {
-        input <<= 64;
-        input += *n;
-    }
-
-    let mut acc : u64 = 0; 
-    let mut count = 0;
- 
-    while !input.is_zero() {
-        let remainder = (input.clone() % BigUint::from(base)).to_u64().unwrap();
-        let bit = bit_table[remainder as usize];
-        acc += bit << count;
-        input /= base;
-        count += 1;
-    }
-
-    let mut repr : <Fr as PrimeField>::Repr = Fr::zero().into_repr();
-    repr.as_mut()[0] = acc;
-    let mut res = Fr::from_repr(repr).expect("should parse");
-
-    res
-}
 
 // Sha256 choose (ch) transform:
 // one of major components of Sha256 hash is bitwise application for a choose function 
@@ -242,34 +112,5 @@ pub fn maj_and_sheduler_xor_ff_normalizer<Fr: PrimeField>(fr: Fr) -> Fr
     let bit_table : [u64; SHA256_MAJORITY_SHEDULER_BASE as usize] =  [ 0, 1, 0, 1 ]; 
     let base = SHA256_MAJORITY_SHEDULER_BASE;
     general_normalizer(fr, &bit_table[..], base)
-}
-
-
-// returns closets upper integer to a / b
-pub fn round_up(a: usize, b : usize) -> usize {
-    let additional_chunks : usize = if a % b > 0 {1} else {0};
-    a/b + additional_chunks
-}
-
-
-pub trait IdentifyFirstLast: Iterator + Sized {
-    fn identify_first_last(self) -> Iter<Self>;
-}
-
-impl<I> IdentifyFirstLast for I where I: Iterator {
-    fn identify_first_last(self) -> Iter<Self> {
-        Iter(true, self.peekable())
-    }
-}
-
-pub struct Iter<I>(bool, iter::Peekable<I>) where I: Iterator;
-
-impl<I> Iterator for Iter<I> where I: Iterator {
-    type Item = (bool, bool, I::Item);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let first = mem::replace(&mut self.0, false);
-        self.1.next().map(|e| (first, self.1.peek().is_none(), e))
-    }
 }
 
