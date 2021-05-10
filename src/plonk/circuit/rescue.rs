942368fcde0fe87451bed82e716ec18cfd725fa2
Alex Vlasov <alex.m.vlasov@gmail.com>
2021-05-10 18:40:35 +0200

    add constraint on permutation gate

--- src/plonk/circuit/rescue.rs
+++ src/plonk/circuit/rescue.rs
@@ -180,6 +180,7 @@ impl<E: Engine> PlonkCsSBox<E> for PowerSBox<E> {
                 let el_term = ArithmeticTerm::from_variable(el.get_variable());
                 term.add_assign(fifth_term);
                 term.sub_assign(el_term);
+                cs.allocate_main_gate(term)?;
 
                 Ok(Num::Variable(out))
             }
